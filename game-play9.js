0d297239776efb8670e4ddc9d6e537c94005f1e8
Milin Mestry <milinmestry@gmail.com>
2017-04-07 14:56:07 +0530

    Added dynamic creation of stars in the function component and css file.

--- game-play9.js
+++ game-play9.js
@@ -1,9 +1,57 @@
+const Stars = (props) => {
+  const numberofStars = 1 + Math.floor(Math.random()*9)
+
+  let stars = []
+  for (let i=0; i<numberofStars; i++) {
+    stars.push(<i key={i} className="fa fa-star"></i>)
+  }
+
+  return(
+    <div className="col-5">
+      {stars}
+    </div>
+  );
+}
+
+const Button = (props) => {
+  return(
+    <div className="col-2">
+      <button className="btn xs-btn">=</button>
+    </div>
+  );
+}
+
+const Answer = (props) => {
+  return(
+    <div className="col-5">
+      ...
+    </div>
+  );
+}
+
+const Numbers = (props) => {
+  return(
+    <div className="card text-center">
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+    </div>
+  );
+}
 
 class Game extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <h3>Play Nine</h3>
+        <hr />
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer />
+        </div>
+        <br />
+        <Numbers />
       </div>
     );
   }
